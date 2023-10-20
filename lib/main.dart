92750bd69cac8b3ab7b370462fa133e5cec7d9a9
Ashish1367 <ayaduvanshi23@gmail.com>
2023-10-20 18:31:44 +0530

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import 'package:food_app/screen/login_page.dart';
 import 'package:food_app/widgets/start_up.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MainApp());
 }
 
