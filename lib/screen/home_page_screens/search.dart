2cb3333a437bc91360c6f143e6866cc0490c50e8
Ashish1367 <ayaduvanshi23@gmail.com>
2023-11-15 11:16:42 +0530

    added search funtionality

--- lib/screen/home_page_screens/search.dart
+++ lib/screen/home_page_screens/search.dart
@@ -1,14 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/widgets/post_card.dart';
 
-class Search extends StatelessWidget {
+class Search extends StatefulWidget {
   const Search({super.key});
 
   @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  final TextEditingController _searchController = TextEditingController();
+  bool showPosts = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Search'),
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: _searchController,
+          decoration: const InputDecoration(labelText: 'Search For A Post'),
+          onFieldSubmitted: (String _) {
+            setState(() {
+              showPosts = true;
+            });
+          },
+        ),
       ),
+      body: showPosts
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('Posts')
+                  .where('recipe', isEqualTo: _searchController.text)
+                  .get(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) =>
+                        PostCard(snap: snapshot.data!.docs[index].data()));
+              },
+            )
+          : const Text('Posts'),
     );
   }
 }
